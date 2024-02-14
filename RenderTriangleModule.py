c6e9448a401175bd86f079a902e87dff19d0b84e
projectpokki <158499943+projectpokki@users.noreply.github.com>
2024-02-14 18:50:56 +0800

    Update RenderTriangleModule.py

--- RenderTriangleModule.py
+++ RenderTriangleModule.py
@@ -1,4 +1,5 @@
 import os
+from math import ceil, floor
 
 
 def renderScreen(screen):
@@ -12,6 +13,7 @@ def renderScreen(screen):
       line += "\033[4" + str(pixel) + "m  "
     print(line)
 
+
 def createScreen(xSize, ySize):
   screen = []
   for y in range(ySize):
@@ -20,15 +22,16 @@ def createScreen(xSize, ySize):
       screen[y].append(0)
   return screen
 
+
 def createTriangle(a, b, c, color, screen):
-  xMin = min([a[0], b[0], c[0]])
-  xMax = max([a[0], b[0], c[0]])
-  yMin = min([a[1], b[1], c[1]])
-  yMax = max([a[1], b[1], c[1]])
+  xMin = floor(min([a[0], b[0], c[0]]))
+  xMax = ceil(max([a[0], b[0], c[0]]))
+  yMin = floor(min([a[1], b[1], c[1]]))
+  yMax = ceil(max([a[1], b[1], c[1]]))
 
   if b[0] == a[0]:
     b, c = c, b
-  
+
   dxb = b[0] - a[0]
   dyb = b[1] - a[1]
   dxc = c[0] - a[0]
